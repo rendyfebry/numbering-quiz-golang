8e300fc37ea4cd634c3ceef13dab1665714604e9
Rendy Febry <rendyfebry@gmail.com>
2020-04-06 18:08:40 +0700

    Implement GetPrimes & GetFibonaccy

--- lib/numbering/numbering.go
+++ lib/numbering/numbering.go
@@ -4,8 +4,8 @@ package numbering
 type Number interface {
 	Sum(x, y int) int
 	Multiply(x, y int) int
-	GetPrimes(n int) ([]int, error)
-	GetFibonnacy(n int) ([]int, error)
+	GetPrimes(n int) []int
+	GetFibonnacy(n int) []int
 }
 
 // NumberSvc ...
@@ -26,12 +26,55 @@ func (*NumberSvc) Multiply(x, y int) int {
 	return x * y
 }
 
+func isPrime(num int) bool {
+	// Fail fast, 0 & 1 is not prime
+	if num < 2 {
+		return false
+	}
+
+	for i := 2; i < num; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func fibonacci(number int) int {
+	if number < 1 {
+		return 0
+	}
+	if number <= 2 {
+		return 1
+	}
+
+	return fibonacci(number-1) + fibonacci(number-2)
+}
+
 // GetPrimes will return n number of primes
-func (*NumberSvc) GetPrimes(n int) ([]int, error) {
-	return []int{}, nil
+func (*NumberSvc) GetPrimes(n int) []int {
+	var primes []int
+	var num = 2 // start with 2
+
+	for len(primes) < n {
+		if isPrime(num) {
+			primes = append(primes, num)
+		}
+
+		num++
+	}
+
+	return primes
 }
 
 // GetFibonnacy will return n number of fibonaccy sequence
-func (*NumberSvc) GetFibonnacy(n int) ([]int, error) {
-	return []int{}, nil
+func (*NumberSvc) GetFibonnacy(n int) []int {
+	var fibs []int
+
+	for i := 0; i < n; i++ {
+		fibs = append(fibs, fibonacci(i))
+	}
+
+	return fibs
 }
--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	num := numbering.NewNumber()
 
-	fmt.Println("Sum(3,4):", num.Sum(3, 4))
-	fmt.Println("Multiply(3,4):", num.Multiply(3, 4))
+	fmt.Println("Sum(3,4): \t\t", num.Sum(3, 4))
+	fmt.Println("Multiply(3,4): \t\t", num.Multiply(3, 4))
+	fmt.Println("GetPrimes(4): \t\t", num.GetPrimes(4))
+	fmt.Println("GetFibonnacy(4): \t", num.GetFibonnacy(4))
 }
